e9cbd826935843ba2d71f4f3c5c5cc94050918a1
jamie <level7jamie@gmail.com>
2024-10-11 22:31:30 +0100

    complete site functionality and accessibility options. Add animal gallery

--- README.md
+++ README.md
@@ -1,25 +1,55 @@
-# accessible-image-gallery
+# Accessible & Responsive Image Gallery
 
 A simple mobile-first image gallery with a focus on responsiveness and accessibility
 
 ## Features
 
-- Full screen image display with gallery automatically set to landscape or portrait depending on device settings.
+- Full screen image display, alternate UI and image sets for portrait and landscape modes, loading automatically upon orientation change.
 - Designed to test srcset attribute - each image exists in several resolutions and automatically switches depending on display size.
-- Ability to choose between differnt categories of image galleries.
+- Ability to choose between different categories of image galleries.
 - Ability to navigate currently selected gallery via keyboard arrow keys or the left and right on-screen arrows. These are clickable or activated via spacebar.
+- When in landscape mode, thumbnail gallery traversable via same methods as above in addition to using tab.
 - Images in current gallery will loop around when they reach the start or end.
-- All interactive sections of the website set to be tabbable and interactive via multiple ways.
+- All interactive sections of the website set to be tabbable and interactive as relevant.
+- Appropriate ARIA roles and labels added as required (on non-semantic tags), with some updating dynamically.
 
 ## Still to do
 
-- Add an icon with tooltip (top right?) explaining functionality and shortcuts. Possibly code to auto-activate upon reaching end of a gallery for the first time?
 - Finish adding the galleries and test it all displays correctly.
 - Ensure all images have appropriate alt tags.
-- Implement category button functionality and styling (along with ability to switch between).
-- Implement accessibility such as ARIA elements and other relevant attributes.
 
 ## Possible feature additions
 
 - More images/gallery categories
 - Increased interactivity via keyboard interaction.
+- Possibility to pull images from the web
+
+# User Requirements
+
+### User Stories
+
+    🐿️ As a user, I want the website design to adjust using media queries so that it works well on both small mobile screens and larger desktop screens.
+    🐿️ As a user with accessibility needs, I expect accessible considerations like alternative text for images so my screen reader can interpret and convey the content to me effectively.
+    🐿️ As a user, I want to easily switch between images in the gallery using buttons so that my experience is smooth and intuitive.
+
+### Requirements
+
+    🎯 Implement responsive design using media queries to ensure the website works well on both small mobile screens and larger desktop screens (e.g., above 800px).
+    🎯 Ensure all images have appropriate alt text attributes for accessibility.
+    🎯 Correctly use event handlers to switch images based on user interactions.
+
+## Stretch Goals
+
+### Stretch User Stories
+
+    🐿️ As a user, I want the website to use 'srcset' to display appropriately sized images for my device, so that images load quickly and look sharp on any screen.
+    🐿️ As a user with accessibility needs, I want ARIA elements, such as aria-label, aria-live, and other relevant attributes, to improve my interaction with the website.
+    🐿️ As a user, I want to use keyboard shortcuts, like arrow keys, to switch between images easily.
+    🐿️ As a user on various devices, I want the website to adapt to different screen sizes through the use of media queries for an optimal viewing experience.
+
+## Stretch Requirements
+
+    🏹 Use 'srcset' to specify which image will be used based on the size of the screen for optimal viewing experience.
+    🏹 Add ARIA elements to improve accessibility, such as aria-label, aria-live, and other relevant attributes.
+    🏹 Implement key bindings for buttons to enhance navigation, such as using arrow keys to switch between images.
+    🏹 Develop media queries for multiple screen sizes.
--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Get DOM nodes
-const selectCategory = document.getElementById("category-btn");
+const selectCategory = document.getElementById("category");
 const prevBtn = document.getElementById("view-prev");
 const nextBtn = document.getElementById("view-next");
 const mainImage = document.getElementById("main-image");
+const tooltip = document.getElementById("tooltip");
 const thumb1 = document.getElementById("1");
 const thumb2 = document.getElementById("2");
 const thumb3 = document.getElementById("3");
@@ -69,6 +70,7 @@ const nature = {
       srcset:
         "./assets/landscape/nature/canyon-sm.jpg 800w, ./assets/landscape/nature/canyon-sm-md.jpg 1200w, ./assets/landscape/nature/canyon-md.jpg 1920w, ./assets/landscape/nature/canyon-lg.jpg 2400w",
       src: "./assets/landscape/nature/canyon-lg.jpg",
+      ariaLabel: this.ariaLabel + "Sunbeam shining through a narrow canyon",
       alt: "Sunbeam shining through a narrow canyon",
     },
     {
@@ -115,35 +117,117 @@ const nature = {
     },
   ],
 };
-const animals = [{ srcset: "", src: "", alt: "" }];
+const animals = {
+  portrait: [
+    {
+      srcset:
+        "./assets/portrait/animals/bird-sm.jpg 800w, ./assets/portrait/animals/bird-sm-md.jpg 1200w, ./assets/portrait/animals/bird-md.jpg 1920w, ./assets/portrait/animals/bird-lg.jpg 2400w",
+      src: "./assets/portrait/animals/bird-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/dog-sm.jpg 800w, ./assets/portrait/animals/dog-sm-md.jpg 1200w, ./assets/portrait/animals/dog-md.jpg 1920w, ./assets/portrait/animals/dog-lg.jpg 2400w",
+      src: "./assets/portrait/animals/dog-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/fox-sm.jpg 800w, ./assets/portrait/animals/fox-sm-md.jpg 1200w, ./assets/portrait/animals/fox-md.jpg 1920w, ./assets/portrait/animals/fox-lg.jpg 2400w",
+      src: "./assets/portrait/animals/fox-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/kangaroo-sm.jpg 800w, ./assets/portrait/animals/kangaroo-sm-md.jpg 1200w, ./assets/portrait/animals/kangaroo-md.jpg 1920w, ./assets/portrait/animals/kangaroo-lg.jpg 2400w",
+      src: "./assets/portrait/animals/kangaroo-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/leopard-sm.jpg 800w, ./assets/portrait/animals/leopard-sm-md.jpg 1200w, ./assets/portrait/animals/leopard-md.jpg 1920w, ./assets/portrait/animals/leopard-lg.jpg 2400w",
+      src: "./assets/portrait/animals/leopard-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/lizard-sm.jpg 800w, ./assets/portrait/animals/lizard-sm-md.jpg 1200w, ./assets/portrait/animals/lizard-md.jpg 1920w, ./assets/portrait/animals/lizard-lg.jpg 2400w",
+      src: "./assets/portrait/animals/lizard-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/owl-sm.jpg 800w, ./assets/portrait/animals/owl-sm-md.jpg 1200w, ./assets/portrait/animals/owl-md.jpg 1920w, ./assets/portrait/animals/owl-lg.jpg 2400w",
+      src: "./assets/portrait/animals/owl-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+    {
+      srcset:
+        "./assets/portrait/animals/yak-sm.jpg 800w, ./assets/portrait/animals/yak-sm-md.jpg 1200w, ./assets/portrait/animals/yak-md.jpg 1920w, ./assets/portrait/animals/yak-lg.jpg 2400w",
+      src: "./assets/portrait/animals/yak-lg.jpg",
+      alt: "A small, rotund bird stood in a puddle",
+    },
+  ],
+  landscape: [
+    {
+      srcset:
+        "./assets/landscape/animals/buffalo-sm.jpg 800w, ./assets/landscape/animals/buffalo-sm-md.jpg 1200w, ./assets/landscape/animals/buffalo-md.jpg 1920w, ./assets/landscape/animals/buffalo-lg.jpg 2400w",
+      src: "./assets/landscape/animals/buffalo-lg.jpg",
+      alt: "A large buffalo looking at the camera",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/camel-sm.jpg 800w, ./assets/landscape/animals/camel-sm-md.jpg 1200w, ./assets/landscape/animals/camel-md.jpg 1920w, ./assets/landscape/animals/camel-lg.jpg 2400w",
+      src: "./assets/landscape/animals/camel-lg.jpg",
+      alt: "Camels walking across desert sand dunes",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/fish-sm.jpg 800w, ./assets/landscape/animals/fish-sm-md.jpg 1200w, ./assets/landscape/animals/fish-md.jpg 1920w, ./assets/landscape/animals/fish-lg.jpg 2400w",
+      src: "./assets/landscape/animals/fish-lg.jpg",
+      alt: "A clown fish swimming by some coral",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/koala-sm.jpg 800w, ./assets/landscape/animals/koala-sm-md.jpg 1200w, ./assets/landscape/animals/koala-md.jpg 1920w, ./assets/landscape/animals/koala-lg.jpg 2400w",
+      src: "./assets/landscape/animals/koala-lg.jpg",
+      alt: "A koala sat on a treebranch hugging another koala",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/lion-sm.jpg 800w, ./assets/landscape/animals/lion-sm-md.jpg 1200w, ./assets/landscape/animals/lion-md.jpg 1920w, ./assets/landscape/animals/lion-lg.jpg 2400w",
+      src: "./assets/landscape/animals/lion-lg.jpg",
+      alt: "A lion roaring at another lion",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/penguin-sm.jpg 800w, ./assets/landscape/animals/penguin-sm-md.jpg 1200w, ./assets/landscape/animals/penguin-md.jpg 1920w, ./assets/landscape/animals/penguin-lg.jpg 2400w",
+      src: "./assets/landscape/animals/penguin-lg.jpg",
+      alt: "A close up of a rockhopper penguin",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/robin-sm.jpg 800w, ./assets/landscape/animals/robin-sm-md.jpg 1200w, ./assets/landscape/animals/robin-md.jpg 1920w, ./assets/landscape/animals/robin-lg.jpg 2400w",
+      src: "./assets/landscape/animals/robin-lg.jpg",
+      alt: "A singing robin",
+    },
+    {
+      srcset:
+        "./assets/landscape/animals/squirrel-sm.jpg 800w, ./assets/landscape/animals/squirrel-sm-md.jpg 1200w, ./assets/landscape/animals/squirrel-md.jpg 1920w, ./assets/landscape/animals/squirrel-lg.jpg 2400w",
+      src: "./assets/landscape/animals/squirrel-lg.jpg",
+      alt: "A red squirrel holding a walnut in its mouth",
+    },
+  ],
+};
 const space = [{ srcset: "", src: "", alt: "" }];
 const art = [{ srcset: "", src: "", alt: "" }];
 const architecture = [{ srcset: "", src: "", alt: "" }];
 const people = [{ srcset: "", src: "", alt: "" }];
 
-// Object containing portrait image categories  USECASE TBD
-const imageCategoriesPortrait = {
-  nature: nature.portrait,
-  animals: animals.portrait,
-  space: space.portrait,
-  art: art.portrait,
-  architecture: architecture.portrait,
-  people: people.portrait,
-};
-
-// Object containing landscape image categories  USECASE TBD
-const imageCategoriesLandscape = {
-  nature: nature.landscape,
-  animals: animals.landscape,
-  space: space.landscape,
-  art: art.landscape,
-  architecture: architecture.landscape,
-  people: people.landscape,
-};
-
-// Initialise current image selection
+// Initialise current image selection and screen orientation
 let imgIndex = 0;
 let currentCategory = nature; // Update this variable to be dynamic & update the relevant functions
+let screenOrientation = getScreenOrientation();
 
 // Switch default image on screen if user is in landscape mode
 if (getScreenOrientation() == "landscape") {
@@ -191,10 +275,19 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-// Put thumbnail nodes into an array to minimise event listeners
+// Automatically switch main image when screen orientation changes
+window.addEventListener("resize", (e) => {
+  if (screenOrientation != getScreenOrientation()) {
+    screenOrientation = getScreenOrientation();
+    imgIndex = 7;
+    nextImg();
+  }
+});
+
+// Put thumbnail nodes into an array to reduce extra event listeners
 const thumbs = [thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8];
 
-// Check all thumbs for click event and update main image accordingly.
+// Check all thumbs for click event or spacebar press and update main image accordingly.
 thumbs.forEach(function (elem) {
   elem.addEventListener("click", (e) => {
     changeImage(elem.id);
@@ -206,24 +299,42 @@ thumbs.forEach(function (elem) {
   });
 });
 
+// Update main image and thumbnails when a new category is selected
+selectCategory.addEventListener("change", (e) => {
+  currentCategory = getCategory();
+  imgIndex = 7;
+  nextImg();
+  let i = 0;
+  for (let thumb of thumbs) {
+    let thumbImg = currentCategory.landscape[i].src;
+    let newStr = thumbImg.slice(0, -6) + "thumb.jpg";
+    thumb.src = newStr;
+    thumb.alt = currentCategory.landscape[i].alt;
+    thumb.ariaLabel = "Thumbnail to open new image: " + thumb.alt;
+    console.log(thumb.ariaLabel);
+    i++;
+  }
+});
+
 // Function to switch to previous image in same array
 function prevImg() {
-  //   let currentCategory = getCategory();
+  currentCategory = getCategory();
   let orientation = getScreenOrientation();
+
   if (imgIndex == 0) {
     imgIndex = currentCategory[orientation].length - 1;
   } else {
     imgIndex--;
   }
-  console.log(imgIndex);
+
   mainImage.src = `${currentCategory[orientation][imgIndex].src}`;
   mainImage.srcset = `${currentCategory[orientation][imgIndex].srcset}`;
   mainImage.alt = `${currentCategory[orientation][imgIndex].alt}`;
-  console.log("code ran");
 }
 
 // Function to switch to next image in same array
 function nextImg() {
+  currentCategory = getCategory();
   let orientation = getScreenOrientation();
 
   if (imgIndex < currentCategory[orientation].length - 1) {
@@ -232,25 +343,39 @@ function nextImg() {
     imgIndex = 0;
   }
 
-  console.log(imgIndex);
   mainImage.src = `${currentCategory[orientation][imgIndex].src}`;
   mainImage.srcset = `${currentCategory[orientation][imgIndex].srcset}`;
   mainImage.alt = `${currentCategory[orientation][imgIndex].alt}`;
-  console.log("code ran");
 }
 
 // Function to change image based on selected thumbnail when in landscape mode
 function changeImage(thumb) {
+  currentCategory = getCategory();
   imgIndex = thumb - 1;
-  console.log(thumb);
-  mainImage.src = `${nature.landscape[imgIndex].src}`;
-  mainImage.srcset = `${nature.landscape[imgIndex].srcset}`;
-  mainImage.alt = `${nature.landscape[imgIndex].alt}`;
+  mainImage.src = `${currentCategory.landscape[imgIndex].src}`;
+  mainImage.srcset = `${currentCategory.landscape[imgIndex].srcset}`;
+  mainImage.alt = `${currentCategory.landscape[imgIndex].alt}`;
 }
 
-// Function to get and return the currently selected image category  STILL TO DO
+// Function to get and return the currently selected category array
 function getCategory() {
-  return;
+  let arraySelect = selectCategory.value;
+  switch (arraySelect) {
+    case "nature":
+      return nature;
+    case "animals":
+      return animals;
+    case "space":
+      return space;
+    case "art":
+      return art;
+    case "architecture":
+      return architecture;
+    case "people":
+      return people;
+  }
+
+  return selectCategory.value;
 }
 
 // Function to get and return the user's current screen orientation
